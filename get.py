fc80685a9e151161fe08e64fd02307955d7a535d
Andrew Stocker <sleepyams@gmail.com>
2020-06-08 15:23:27 -0600

    get participants and format
    blahblah

--- get.py
+++ get.py
@@ -19,8 +19,23 @@ def get_tournament(tournament_id):
     r = requests.get(url)
     return r.json()
 
+def dump_tournaments():
+    for tournament_id in tournament_ids:
+        filename = "data/{id}.json".format(id=tournament_id)
+        with open(filename, "w") as f:
+            tournament_data = get_tournament(tournament_id)
+            json.dump(tournament_data, f, indent=4, sort_keys=True)
 
-for tournament_id in tournament_ids:
-    tournament_data = get_tournament(tournament_id)
-    with open("data/{id}.json".format(id=tournament_id), "w") as f:
-        json.dump(tournament_data, f)
+
+def get_tournament_participants(tournament_id):
+    path = "tournaments/{id}/participants.json"
+    url = API_URL + path.format(id=tournament_id)
+    r = requests.get(url)
+    return r.json()
+
+def dump_tournament_participants():
+    for tournament_id in tournament_ids:
+        filename = "data/{id}_participants.json".format(id=tournament_id)
+        with open(filename, "w") as f:
+            participants_data = get_tournament_participants(tournament_id)
+            json.dump(participants_data, f, indent=4, sort_keys=True)
---
+++ main.py
@@ -0,0 +1,6 @@
+from get import dump_tournaments, dump_tournament_participants
+
+
+if __name__ == "__main__":
+    dump_tournaments()
+    dump_tournament_participants()
